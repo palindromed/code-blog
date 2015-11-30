5cd7ac117ff537f101171da7f0e3c92e3e96bff6
Hannah Krager <hannahkrager@gmail.com>
2015-11-30 14:32:25 -0800

    blog is object of rawdata

--- scripts/article.js
+++ scripts/article.js
@@ -1,17 +1,12 @@
 'use strict';
-(function() {
 
+var Article = function(props){
+  this.author = props.author;
+  this.title = props.title;
+  this.body = props.body;
+  this.publishedOn = props.publishedOn;
+};
 
-  var Article = function(props){
-    this.author = props.author;
-    this.title = props.title;
-    this.body = props.body;
-    this.publishedOn = props.publishedOn;
-  }
-  Article.prototype.toHtml = function () {
-    $('h1.postTitle').text = this.title;
-    $('p.postInfo').text =  'By ' this.author + ' on ' + this.publishedOn;
-    $('p.postBody').text = this.body;
+Article.prototype.toHtml = function () {
 
-  };
-})();
+};
