b32bac6cfdbea9a659062707777708cd65112c5e
Hannah Krager <hannahkrager@gmail.com>
2015-12-01 14:59:08 -0800

    finalize day 1 code

--- scripts/blog.js
+++ scripts/blog.js
@@ -4,12 +4,27 @@ var blog = {};
 
 blog.makePosts = function(){
   for(var i=0; i < blog.rawData.length; i++){
+    blog.sortArticles();
     var post = blog.rawData[i];
     post.daysBetween = blog.dateDiff(post.publishedOn);
     var posted =new Article(post);
     posted.toHtml();
   }
 };
+
+
+
+blog.sortArticles = function () {
+  blog.rawData.sort(function (a, b) {
+  if (a.value > b.value) {
+    return 1;
+  }
+  if (a.value < b.value) {
+    return -1;
+  }
+  return 0;
+  });
+}
 
 blog.dateDiff = function(date1){
   var today = new Date();
