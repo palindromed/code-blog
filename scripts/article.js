09a91e5043acf7c046da74a243e4616adc9fe687
Hannah Krager <hannahkrager@gmail.com>
2015-11-30 17:23:27 -0800

    working rendering, single column. Not sorted

--- scripts/article.js
+++ scripts/article.js
@@ -8,10 +8,9 @@ var Article = function(props){
 };
 
 Article.prototype.toHtml = function () {
-  var $clone = $('.template').clone(true);
+  var $clone = $(' .template').filter(':first').clone(true);
   $clone.find('.author').html(this.author);
   $clone.find('.body').html(this.body);
   $clone.find('.title').html(this.title);
-  //$clone.find('.body .title .template .author').removeClass('.template');
-  $('main').html($clone);
+  $('.main').append($clone);
 };
--- scripts/blog.js
+++ scripts/blog.js
@@ -4,6 +4,7 @@ var blog = {};
 var makePosts = function(){
   for(var i=0; i < blog.rawData.length; i++){
     var post = new Article(blog.rawData[i]);
+    console.log(post);
     post.toHtml();
   }
 
