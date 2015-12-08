f86fe7b0a34d884a11d3379c93600f57be52545c
Hannah Krager <hannahkrager@gmail.com>
2015-12-08 14:14:28 -0800

    handle html in body, count words and keep with author info

--- scripts/stats.js
+++ scripts/stats.js
@@ -1,19 +1,42 @@
 var stats = {};
-var newData = JSON.parse(localStorage.getItem('Articles'));
+stats.posts = JSON.parse(localStorage.getItem('Articles'));
 
 stats.checkBlog = function() {
 
   var articleCount = stats.countArticles();
   $('#num-articles').text('Article Count: ' + articleCount);
   //stats.renderStats();
+  stats.stripHtml();
 
 
 };
 
 stats.countArticles = function() {
-  return newData.length;
+  return stats.posts.length;
 };
 
+stats.stripHtml = function(){
+  var words = [];
+  stats.posts.forEach(function(value, i){
+
+    var html = value.body;
+    var div = document.createElement('div');
+    div.innerHTML = html;
+    var text = div.textContent || div.innerText || '';
+    value.wordCount = stats.countWords(text);
+    //console.log(value.wordCount);
+
+
+  });
+};
+
+stats.countWords = function(words){
+
+  var wordList = words.split(' ');
+  return wordList.length;
+
+
+}
 
 
 stats.renderStats = function(){
