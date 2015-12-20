9dae4e6a10b3fea9bf1ff060e31d07fe8c304baf
Hannah Krager <hannahkrager@gmail.com>
2015-12-20 12:14:09 -0800

    trouble with about page

--- scripts/repo.js
+++ scripts/repo.js
@@ -7,7 +7,7 @@ repos.requestAll = function(callback){
   $.ajax({
     type: 'GET',
     url: 'https://api.github.com/user/repos?sort=updated',
-    headers: {Authorization: 'token ' + GITHUB_TOKEN }
+    
   }).done(function(data){
     repos.all = data;
   }).done(callback);
