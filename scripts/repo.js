49b25f6d0162c5022591abd815eda5eb173d703d
Hannah Krager <hannahkrager@gmail.com>
2015-12-20 13:01:24 -0800

    proper response, not rendering

--- scripts/repo.js
+++ scripts/repo.js
@@ -7,6 +7,8 @@ repos.requestAll = function(callback){
   $.ajax({
     type: 'GET',
     url: '/github/user/repos?sort=updated'
+  }).done(function(data){
+    repos.all = data;
   }).done(callback);
 
 };
