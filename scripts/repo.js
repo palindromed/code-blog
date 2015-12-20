a7f134e45d9b652d5c1fada430de77a8eaae677f
Hannah Krager <hannahkrager@gmail.com>
2015-12-20 12:55:39 -0800

    change request

--- scripts/repo.js
+++ scripts/repo.js
@@ -6,10 +6,7 @@ repos.requestAll = function(callback){
 
   $.ajax({
     type: 'GET',
-    url: '/github/user/repos?sort=updated',
-
-  }).done(function(data){
-    repos.all = data;
+    url: '/github/user/repos?sort=updated'
   }).done(callback);
 
 };
