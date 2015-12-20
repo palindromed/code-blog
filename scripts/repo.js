ade7c93876b9584c0139d199e39a82983a39a513
Hannah Krager <hannahkrager@gmail.com>
2015-12-20 12:01:28 -0800

    fix env

--- scripts/repo.js
+++ scripts/repo.js
@@ -7,7 +7,7 @@ repos.requestAll = function(callback){
   $.ajax({
     type: 'GET',
     url: 'https://api.github.com/user/repos?sort=updated',
-    headers: {Authorization: 'token ' + ENV['githubToken'] }
+    headers: {Authorization: 'token ' + process.env.GITHUB_TOKEN }
   }).done(function(data){
     repos.all = data;
   }).done(callback);
